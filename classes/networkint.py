b8be2d9ab2715a72ea2a9c2c29acd0b700c58ee4
ParamKalaria <paramkalariya.32@gmail.com>
2025-06-13 00:07:36 +0530

    networkint code imporved

--- classes/networkint.py
+++ classes/networkint.py
@@ -9,17 +9,23 @@ def list_network_interfaces():
         mac_address = "N/A"  # Default MAC address placeholder
 
         for addr in addresses:
-            if addr.family.name == "AF_LINK":  # MAC Address
+            family = "Unknown"
+            if addr.family.name == "AF_INET":  # IPv4
+                family = "IPv4"
+            elif addr.family.name == "AF_INET6":  # IPv6
+                family = "IPv6"
+            elif addr.family.name == "AF_LINK":  # MAC Address
                 mac_address = addr.address
-            else:  # IP Addresses (IPv4 or IPv6)
-                table_data.append([
-                    interface,
-                    addr.family.name,
-                    addr.address,
-                    addr.netmask if addr.netmask else "N/A",
-                    mac_address  # MAC Address is displayed separately
-                ])
+                continue  # Skip adding MAC address to IP rows
 
-    headers = ["Interface", "Family", "Address", "Netmask", "MAC Address"]
-    return tabulate(table_data, headers, tablefmt="grid")
+            table_data.append([
+                interface,
+                family,
+                addr.address,
+                addr.netmask if addr.netmask else "N/A",
+                mac_address  # MAC Address is displayed separately
+            ])
+
+    headers = ["Interface", "Type", "Address", "Netmask", "MAC Address"]
+    print(tabulate(table_data, headers, tablefmt="grid"))
 
